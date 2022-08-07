c0de9d835a8c652080ea5e278488aa4c50de1c7e
Tural Guliyev <tguliev9@gmail.com>
2022-08-08 00:11:16 +0400

    check

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Algorithms;
 public static class Program {
     public static void Main(string[]? args) {
-        int[] arr = new int[]{-34, 0, 1, 4, 6, 24, 33, 55, 234};
+        int[] arr = new int[]{-34, 0, 1, 4, 6, 24, 33, 55, 2334};
         Console.WriteLine(BinarySearch.binarySearch(arr, 24));
     }
 }
